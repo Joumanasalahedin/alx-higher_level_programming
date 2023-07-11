74d0528595c312b766025ee14394319e4bb5e7f7
Joumana <joumanasalahedin@gmail.com>
2023-07-11 23:36:50 +0200

    task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -4,9 +4,3 @@ def print_last_digit(number):
     if number < 0:
         last_digit = -last_digit
     print(last_digit)
-
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
