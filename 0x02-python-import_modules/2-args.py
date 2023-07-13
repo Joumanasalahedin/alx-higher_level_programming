d7e884462579052ebd54878645b2d2b3ac15e433
Joumana <joumanasalahedin@gmail.com>
2023-07-14 01:12:29 +0200

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-from sys import argv
-length = len(argv)
-if length == 1:
+if __name__ == "__main__":
+    from sys import argv
+length = len(argv) - 1
+if length == 0:
     print("0 arguments.")
 else:
-    if length == 2:
-        print(f"{length - 1} argument:")
+    if length == 1:
+        print(f"{length} argument:")
     else:
-        print(f"{length - 1} arguments:")
-    for idx in range(1, length):
-        print(f"{idx}: {argv[idx]}")
+        print(f"{length} arguments:")
+    idx = 0
+    for arg in argv:
+        if idx != 0:
+            print(f"{idx}: {arg}")
+        idx += 1
