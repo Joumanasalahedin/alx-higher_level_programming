c2ab727ce787d3c4f71ad59f7445495499bab30f
Joumana <joumanasalahedin@gmail.com>
2023-07-17 01:41:04 +0200

    task 4

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(my_list):
+        return new_list
+
+    new_list[idx] = element
+    return new_list
+
+
+my_list = [1, 2, 3, 4, 5]
+idx = 3
+new_element = 9
+new_list = new_in_list(my_list, idx, new_element)
+
+print(new_list)
+print(my_list)
