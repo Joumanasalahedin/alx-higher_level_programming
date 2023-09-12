bffb942a0385cc82de2dc4d46df0d3ee341fc322
Joumana <joumanasalahedin@gmail.com>
2023-09-12 21:51:05 +0200

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
+""" Module with function that reads from a file"""
+
+
 def read_file(filename=""):
     """
     Function that reads from a file and prints data
     """
-    with open(filename, encoding='utf-8') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         read_data = file.read()
         print(read_data, end='')
