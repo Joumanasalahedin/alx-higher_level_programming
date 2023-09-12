33cca8afd0fc9f7944bee4a3978e2f53fb6b4293
Joumana <joumanasalahedin@gmail.com>
2023-09-12 22:35:53 +0200

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+"""Module with function that returns JSOn representation"""
+
+
+def to_json_string(my_obj):
+    """
+    Function that returns JSON representation of an object (string)
+    """
+    return json.dumps(my_obj)
