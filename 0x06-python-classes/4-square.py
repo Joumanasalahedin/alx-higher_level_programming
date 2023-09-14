844488cafda8a961ae35a22451e5cfeaff610c7c
Joumana <joumanasalahedin@gmail.com>
2023-09-14 15:42:50 +0200

    task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""Define a class Square"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square
+        Args:
+        size (int): size of the new square
+        """
+        self.size = size
+
+    def size(self):
+        """Retrieve square size"""
+        return self.__size
+
+    def size(self, value):
+        """Set square size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return self.__size ** 2
