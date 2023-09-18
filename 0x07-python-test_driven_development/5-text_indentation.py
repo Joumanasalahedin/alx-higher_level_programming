e140e10bce045834328f181f23718a3ee683a7e9
Joumana <joumanasalahedin@gmail.com>
2023-09-18 03:06:22 +0200

    task 4

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Module with function that prints a text with 
+2 new lines after certain characters """
+
+
+def text_indentation(text):
+    """
+    Function that prints a text with 2 new lines after
+    . ? and :
+
+    Args:
+    text: string
+    """
+
+    if type(text) is not str:
+        raise TypeError("text must be a string")
+
+    for d in ".?:":
+        text = (d + "\n\n").join(
+            [line.strip(" ") for line in text.split(d)])
+
+    print(f"{text}", end="")
