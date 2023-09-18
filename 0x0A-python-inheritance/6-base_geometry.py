3620f5081225ce47a251509d80c9ff4d90b87e80
Joumana <joumanasalahedin@gmail.com>
2023-09-19 01:45:56 +0200

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for class BaseGeometry"""
+
+
+class BaseGeometry:
+    """Class BaseGeometry"""
+
+    def area(self):
+        """Method that raises an Exception"""
+        raise Exception("area() is not implemented")
