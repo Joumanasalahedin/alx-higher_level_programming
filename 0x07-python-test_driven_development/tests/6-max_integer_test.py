f48884652a83897d190521b89995e94898648354
Joumana <joumanasalahedin@gmail.com>
2023-09-18 03:44:24 +0200

    task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,9 +10,14 @@ class TestMaxInteger(unittest.TestCase):
 
     def test_max(self):
         self.assertEqual(max_integer([2, 5, 10]), 10)
+        self.assertEqual(max_integer[5], 5)
 
     def test_empty(self):
         self.assertEqual(max_integer([]), None)
 
     def test_negative(self):
         self.assertEqual(max_integer([0, -15, 25, -5, 17]), 25)
+        self.assertEqual(max_integer([-5, -15, -7, -13]), -5)
+
+    def test_type(self):
+        self.assertRaises(TypeError, max_integer, 7)
