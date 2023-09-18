aefd6a8f513c03de2eb2fa3fdb28ea07ed68dbbb
Joumana <joumanasalahedin@gmail.com>
2023-09-19 01:41:38 +0200

    task 4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module with function that tests if the object is an
+instance of a class that inherited from the specified class"""
+
+
+def inherits_from(obj, a_class):
+    """Function that returns True if the object is an instance of 
+    a class that inherited (directly or indirectly) from the 
+    specified class ; otherwise False"""
+    if issubclass(type(obj), a_class) and type(obj) != a_class:
+        return True
+    return False
