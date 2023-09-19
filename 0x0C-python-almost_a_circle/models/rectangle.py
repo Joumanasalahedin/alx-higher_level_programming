02953e789d75649348a2f62a1e7693fd1afadef4
Joumana <joumanasalahedin@gmail.com>
2023-09-19 23:59:38 +0200

    task 2+3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-"""Module with class Rectangle inheirted from Base"""
+"""Module that contains class Rectangle,
+inheritance of class Base"""
 
 from models.base import Base
 
 
 class Rectangle(Base):
-    """class Rectangle inherited from Base"""
+    """Class Rectangle"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initializes instance"""
