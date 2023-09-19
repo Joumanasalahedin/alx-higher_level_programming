92181c2bdd36220c873a49585dac776a740e8ffd
Joumana <joumanasalahedin@gmail.com>
2023-09-19 14:05:44 +0200

    task 2+3

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,2 +1,72 @@
 #!/usr/bin/python3
 """Module with class Rectangle inheirted from Base"""
+
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle inherited from Base"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initializes instance"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Retrieve rectangle width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set rectangle width"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value <= 0:
+            raise ValueError("width must be > 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Retrieve rectangle height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set rectangle height"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value <= 0:
+            raise ValueError("height must be > 0")
+        self.__height = value
+
+    @property
+    def x(self):
+        """x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """x setter"""
+        if type(value) != int:
+            raise TypeError("x must be an integer")
+        elif value < 0:
+            raise ValueError("x must be >= 0")
+        self.__x = value
+
+    @property
+    def y(self):
+        """y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """y setter"""
+        if type(value) != int:
+            raise TypeError("y must be an integer")
+        elif value < 0:
+            raise ValueError("y must be >= 0")
+        self.__y = value
