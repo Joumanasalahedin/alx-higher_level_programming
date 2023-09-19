5265dcab08c09e3bba06f74d5b07c21b4053f00a
Joumana <joumanasalahedin@gmail.com>
2023-09-20 00:26:18 +0200

    rectangle tasks fix

--- 0x0C-python-almost_a_circle/tests/test_base.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-"""Module for test Base class"""
-import io
-import sys
-import unittest
-from models.base import Base
---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for test Base class"""
+import io
+import sys
+import unittest
+from models.base import Base
+
+
+class TestBaseMethods(unittest.TestCase):
+    """ Suite to test Base class """
+
+    def setUp(self):
+        """ Method invoked for each test """
+        Base._Base__nb_objects = 0
+
+    def test_id(self):
+        """ Test assigned id """
+        new = Base(1)
+        self.assertEqual(new.id, 1)
