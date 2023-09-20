200766161e0d754859389efcf2feb645269195ed
Joumana <joumanasalahedin@gmail.com>
2023-09-20 22:33:30 +0200

    task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module that contains class Square,
+inheritance of class Rectangle"""
+
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Class Square"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Initializes instance"""
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """special str method"""
+        id = f"({self.id})"
+        xy = f"{self.x}/{self.y}"
+        size = f"{self.width}"
+        return f"[Square] {id} {xy} - {size}"
