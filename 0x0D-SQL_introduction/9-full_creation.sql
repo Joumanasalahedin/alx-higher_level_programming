e968f9add4da0bdb2d8016f540d526e2719a9713
Joumana <joumanasalahedin@gmail.com>
2023-10-17 23:57:30 +0200

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- Creates a new table with data
 CREATE TABLE IF NOT EXISTS second_table (
 id INT,
-name VARCHAR (256),
+name VARCHAR(256),
 score INT);
 INSERT INTO second_table ('id', 'name', 'score') VALUES (1, 'John', 10);
 INSERT INTO second_table ('id', 'name', 'score') VALUES (2, 'Alex', 3);
