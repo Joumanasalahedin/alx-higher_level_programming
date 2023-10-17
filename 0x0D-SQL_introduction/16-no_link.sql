2dca1abc504858c6f5f2dff329bc2470fc2b890d
Joumana <joumanasalahedin@gmail.com>
2023-10-18 00:21:34 +0200

    task 16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,2 +1,2 @@
 -- Lists all records
-SELECT score, name FROM second_table WHERE id IS NOT NULL ORDER BY score DESC, name;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC, name;
