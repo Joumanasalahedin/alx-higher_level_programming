e9995223e2a79f750d128c2bf91331bb79f99a3a
Joumana <joumanasalahedin@gmail.com>
2023-10-18 20:41:34 +0200

    task 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,5 +2,5 @@
 SELECT shows.title, genres.genre_id FROM tv_shows AS shows
     LEFT JOIN tv_show_genres AS genres
     ON shows.id = genres.show_id
-    WHERE shows.genre_id IS NULL
+    WHERE genres.genre_id IS NULL
 ORDER BY shows.title, genres.genre_id;
