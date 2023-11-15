68d3c1efc3f9ca5e4796979cd2e9b70c6b9e0b3e
Joumana <joumanasalahedin@gmail.com>
2023-11-16 00:18:22 +0100

    task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let elem = 0;
-  for (i in list) {
+  for (const i in list) {
     if (list[i] === searchElement) {
       elem += 1;
     }
