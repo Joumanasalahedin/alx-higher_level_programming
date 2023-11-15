003175e51fe0247312d8f48395a8dac2d0d887b1
Joumana <joumanasalahedin@gmail.com>
2023-11-15 12:50:52 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,8 @@
 #!/usr/bin/node
 if (!process.argv[3] || process.argv[3] === 1) {
-    console.log(0)
-}
-else {
-    const args = process.argv.slice(2).map(Number)
-    const sorted = args.sort((a, b) => b - a)
-    console.log(sorted[1])
+  console.log(0);
+} else {
+  const args = process.argv.slice(2).map(Number);
+  const sorted = args.sort((a, b) => b - a);
+  console.log(sorted[1]);
 }
