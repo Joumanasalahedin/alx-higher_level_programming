801cd66fb3e992778f4e709da649f4560ce859e8
Joumana <joumanasalahedin@gmail.com>
2023-11-15 12:29:41 +0100

    task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-function factorial(num) {
-    if (num === 0 || isNaN(num)) {
-        return (1)
-    }
-    else {
-        return (num * factorial(num - 1))
-    }
+function factorial (num) {
+  if (num === 0 || isNaN(num)) {
+    return (1);
+  } else {
+    return (num * factorial(num - 1));
+  }
 }
-console.log(factorial(parseInt(process.argv[2])))
+console.log(factorial(parseInt(process.argv[2])));
