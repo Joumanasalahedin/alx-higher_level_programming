d6f6ead0682a31401910547d3d990606f476d824
Joumana <joumanasalahedin@gmail.com>
2023-11-15 11:38:14 +0100

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
 if (process.argv[2]) {
   const num = parseInt(process.argv[2]);
-  console.log('My number: ' + num);
+  if (isNaN(num)) {
+    console.log('Not a number');
+  } else {
+    console.log('My number: ' + num);
+  }
 } else {
   console.log('Not a number');
 }
