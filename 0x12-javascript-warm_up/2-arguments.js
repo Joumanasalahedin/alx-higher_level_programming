59a2d0b5c7e136a3865b85703178c88c6fa62a02
Joumana <joumanasalahedin@gmail.com>
2023-11-15 01:54:48 +0100

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,8 @@
 #!/usr/bin/node
 if (process.argv.length - 2 == 0) {
     console.log('No argument');
-}
-else if (process.argv.length - 2 == 1) {
+} else if (process.argv.length - 2 == 1) {
     console.log('Argument found');
-}
-else {
+} else {
     console.log('Arguments found');
 }
