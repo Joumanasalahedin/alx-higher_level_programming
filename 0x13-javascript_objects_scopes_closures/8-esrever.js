516339596d260c4f0e9b1b59e16130c33af52b23
Joumana <joumanasalahedin@gmail.com>
2023-11-16 00:26:07 +0100

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-    const rev_arr = [];
+  const revArray = [];
 
-    for (let i = list.length - 1; i >= 0; i--) {
-        rev_arr.push(list[i]);
-    }
-    return rev_arr
-}
+  for (let i = list.length - 1; i >= 0; i--) {
+    revArray.push(list[i]);
+  }
+  return revArray;
+};
