df8a3396bd7fb685549d302ae233ddadf30e1e31
Joumana <joumanasalahedin@gmail.com>
2023-11-30 21:51:49 +0100

    task 8

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a JSON POST request and displays response
-curl -sX POST -H 'Content-Type: application/json' -d "$2" "$1"
+curl -sX POST -H 'Content-Type: application/json' -d "$(cat "$2")" "$1"
