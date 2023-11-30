a89583e783a06aa0826880195186ae840755d8d7
Joumana <joumanasalahedin@gmail.com>
2023-11-30 21:19:47 +0100

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # sends a POST request and displays response
-curl --data "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -s --data "email=test@gmail.com&subject=I will always be here for PLD" "$1"
