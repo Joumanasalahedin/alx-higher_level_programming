b015233615a7d003009ca0ee3fcae6dca05bd85a
Joumana <joumanasalahedin@gmail.com>
2023-11-30 22:20:02 +0100

    task 9

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request that responds witha specific message
-curl -sLX PUT --data "user_id=98" 0.0.0.0:5000/catch_me
+curl -sLX PUT -H "Origin: HolbertonSchool" --data "user_id=98" 0.0.0.0:5000/catch_me
