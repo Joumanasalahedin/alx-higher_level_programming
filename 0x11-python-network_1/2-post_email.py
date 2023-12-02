5e0f24452f614123b9e9cbe7da7390e9b63e0c89
Joumana <joumanasalahedin@gmail.com>
2023-12-02 01:06:27 +0100

    task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -7,7 +7,7 @@ import urllib.parse
 url = argv[1]
 email = argv[2]
 
-data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+data = urllib.parse.urlencode({'email': email}).encode('ascii')
 request = urllib.request.Request(url, data)
 with urllib.request.urlopen(request) as response:
     print(response.read().decode('utf-8'))
