39bd12a8a5f627a624923ab9ce6ac3b597396164
Joumana <joumanasalahedin@gmail.com>
2023-12-02 02:31:09 +0100

    task 10

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Lists the 10 most recent commits on a given GitHub repository."""
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    repo = argv[1]
+    owner = argv[2]
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/commits"
+
+    r = requests.get(url)
+    for i in range(10):
+        print(f"{r.get('sha')}: {r.get('commit').get('author').get('name')}")
