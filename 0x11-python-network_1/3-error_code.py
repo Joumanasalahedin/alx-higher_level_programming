ce7799373855683c7d0dad6e5314cfbda420a9d2
Joumana <joumanasalahedin@gmail.com>
2023-12-02 01:26:44 +0100

    task 3

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+# /usr/bin/python3
+"""sends a request to URL and displays body"""
+from sys import argv
+import urllib.request
+import urllib.error
+
+if __name__ == "__main__":
+    request = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(request) as response:
+            body = response.read()
+            print(body.decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print(f"Error code: {error}")
