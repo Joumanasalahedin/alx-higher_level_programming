9b7e792c8fc23df08d2a99dcb1264aeac12bcfdd
Joumana <joumanasalahedin@gmail.com>
2024-01-06 23:06:08 +0100

    task 0

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2]
+const file = process.argv[2];
 fs.readFile(file, 'utf-8', (err, body) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
-    console.log(body);
-})
+  if (err) {
+    console.error(err);
+    return;
+  }
+  console.log(body);
+});
