8e6751b870cc8aabcee757c7e0b056f207003959
Joumana <joumanasalahedin@gmail.com>
2024-01-07 02:25:04 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -2,17 +2,15 @@
 const request = require('request');
 const apiURL = process.argv[2]; // Get the API URL from command line argument
 
-
 request(apiURL, { json: true }, (err, res, body) => {
-    const wedgeAntillesId = 18;
-    let count = 0;
+  const wedgeAntillesId = 18;
+  let count = 0;
 
-    body.results.forEach(film => {
-        if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
-            count++;
-        }
-    });
+  body.results.forEach(film => {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeAntillesId}/`)) {
+      count++;
+    }
+  });
 
-    console.log(count);
+  console.log(count);
 });
-
