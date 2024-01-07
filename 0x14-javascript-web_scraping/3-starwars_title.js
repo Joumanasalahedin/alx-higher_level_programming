5f9c1e9542ea0ec4de3026cb014ff35654ccffbf
Joumana <joumanasalahedin@gmail.com>
2024-01-07 02:11:58 +0100

    task 3

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const request = require('request');
+const movieid = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${id}`
+
+request(url, function (error, response, body) {
+    if (error == null) {
+        const json = JSON.parse(body);
+        console.log(json.title)
+    }
+})
