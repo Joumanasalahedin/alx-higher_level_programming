97006892d9e7d74330a354d4a23d08d45d261689
Joumana <joumanasalahedin@gmail.com>
2024-01-07 02:49:22 +0100

    task 5

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const file = process.argv[3];
+
+request(process.argv[2], function (err, response, body) {
+    if (err == null) {
+        fs.writeFileSync(file, body);
+    }
+});
